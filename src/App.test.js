1726f963eb69acfb00ad577063e1e999383bd08f
Apurv Patel <apurv9879@gmail.com>
2023-11-05 11:10:05 +0530

    changes made

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import expect from "expect";
+
+test('renders learn react link', () => {
+    const { getByText } = render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
+
+    // eslint-disable-next-line no-restricted-globals
+    expect(screen.getByText(/learn/i)).toBeInTheDocument();
+});
